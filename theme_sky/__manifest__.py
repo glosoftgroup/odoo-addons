c67dd7326b732a36045002eac833b5dc0d6c9ecb
p8ul <pkinuthia10@gmail.com>
2017-05-04 06:18:06 -0700

    First commit

---
+++ theme_sky/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sky Theme',
+    'summary': 'Support for Bootswatch themes in master',
+    'description': 'This theme module is exclusively for master to keep the support of Bootswatch themes which were previously part of the website module in 8.0.',
+    'category': 'Theme',
+    'sequence': 900,
+    'version': '1.0',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'data/theme_bootswatch_data.xml',
+        'views/theme_bootswatch_templates.xml','views/layout.xml','views/pages.xml',
+        'views/assets.xml',
+        'views/snippets.xml',
+        'views/options.xml',
+    ],
+    'images': ['static/description/bootswatch.png'],
+    'application': True,
+}
